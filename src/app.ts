69b3760e67f590acd5bdfef775fc75ddfd73e828
lmafia <lmafiamm@gmail.com>
2024-07-11 00:11:52 +0800

    Class and  constructor

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,10 @@
-function sum(x: number, y: number, z: number): number {
-    return x + y + z;
+class Department {
+    name: string;
+
+    constructor (name: string) {
+        this.name = name;
+    }
 }
 
-const numbers: [number, number, number] = [1, 2, 3];
-
-console.log(sum(...numbers)); // 等同于 sum(1, 2, 3)
-
-const printDetails = (name: string, age: number) => {
-    console.log(`${name} is ${age} years old`);
-}
-
-let person = { name: 'Alice', age: 30 };
-
-let { a, ...rest } = { a: 1, b: 2, c: 3 };
-console.log(a);
-console.log(rest);
-
-
-function sumRest(...rest: number[]): number {
-    return rest.reduce(
-        (acc, currentValue) =>
-            acc + currentValue, 0);
-}
-console.log(`rest-array: ${sumRest(1, 2, 3)}`);
-console.log(`rest-rest: ${sumRest(...numbers)}`);
-
+const accounting = new Department('Accounting')
+console.log(accounting);
