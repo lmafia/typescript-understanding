5ba3203a1e59cb142c7b5ef96f202038c0bcd3cc
lmafia <lmafiamm@gmail.com>
2024-07-11 12:04:18 +0800

    类的成员方法和 this 的用处

--- dist/app.js
+++ dist/app.js
@@ -1,29 +1,25 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
-function sum(x, y, z) {
-    return x + y + z;
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+    // 保证了 this 永远是指向当前实例
+    process() {
+        console.log(`${this.name} processing`);
+    }
+    // 添加了这个方法, 就允许丢失 this 的情况
+    // 所以上面的写法可以在编程阶段减少错误出现
+    processMissThis() {
+        console.log(`${this.name} processing`);
+    }
 }
-const numbers = [1, 2, 3];
-console.log(sum(...numbers)); // 等同于 sum(1, 2, 3)
-const printDetails = (name, age) => {
-    console.log(`${name} is ${age} years old`);
+const accounting = new Department('Accounting');
+console.log(accounting);
+accounting.process();
+const development = {
+    // name: "Development",
+    process: accounting.process,
+    processMissThis: accounting.processMissThis
 };
-let person = { name: 'Alice', age: 30 };
-let _a = { a: 1, b: 2, c: 3 }, { a } = _a, rest = __rest(_a, ["a"]);
-console.log(a);
-console.log(rest);
-function sumRest(...rest) {
-    return rest.reduce((acc, currentValue) => acc + currentValue, 0);
-}
-console.log(`rest-array: ${sumRest(1, 2, 3)}`);
-console.log(`rest-rest: ${sumRest(...numbers)}`);
+development.processMissThis();
+console.log(development);
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,31 @@ class Department {
     constructor (name: string) {
         this.name = name;
     }
+
+
+    // 保证了 this 永远是指向当前实例
+    process(this: Department) {
+        console.log(`${this.name} processing`);
+    }
+
+    // 添加了这个方法, 就允许丢失 this 的情况
+    // 所以上面的写法可以在编程阶段减少错误出现
+    processMissThis() {
+        console.log(`${this.name} processing`);
+    }
 }
 
 const accounting = new Department('Accounting')
 console.log(accounting);
+accounting.process();
+
+
+const development = {
+    // name: "Development",
+    process: accounting.process,
+    processMissThis: accounting.processMissThis
+};
+
+development.processMissThis();
+
+console.log(development);
