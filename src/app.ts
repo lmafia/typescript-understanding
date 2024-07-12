0d9b3bbf678978af0504b34356a5def3fe476f9b
lmafia <lmafiamm@gmail.com>
2024-07-13 00:14:31 +0800

    setter  getter

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,50 @@
-class Person {
-    private readonly name: string;
+class Thing {
+    private _size = 0;
 
-    constructor (name: string) {
-        this.name = name;
+    get size(): number {
+        return this._size;
     }
 
-    changeName(newName: string) {
-        // 错误：Cannot assign to 'name' because it is a read-only property.
-        // this.name = newName;
+    set size(value: string | number | boolean) {
+        let num = Number(value);
+
+        // Don't allow NaN, Infinity, etc
+
+        if (!Number.isFinite(num)) {
+            this._size = 0;
+            return;
+        }
+
+        this._size = num;
     }
-    getName(): string {
-        return this.name;
+}
+let thing = new Thing()
+// setter  getter 不用括号, 不像是函数，像是一个属性
+thing.size = 1;
+console.log(thing.size);
+
+
+class Employee {
+    private _fullName: string = '';
+
+    // getter
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    // setter
+    set fullName(newName: string) {
+        if (newName && newName.trim()) {
+            this._fullName = newName;
+        } else {
+            throw Error('Invalid name provided.');
+        }
     }
 }
 
-let person = new Person('Alice');
-console.log(person.getName()); // 输出: Alice
+let employee = new Employee();
+employee.fullName = 'John Doe'; // 设置 fullName，调用 setter
+console.log(employee.fullName); // 获取 fullName，调用 getter，输出: John Doe
 
-// 错误：Cannot assign to 'name' because it is a read-only property.
-// person.name = 'Bob';
+employee.fullName = ''; // 设置一个无效的 fullName，
+console.log(employee.fullName); // 
