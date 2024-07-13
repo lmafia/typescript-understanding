b08e88f6cfa3dfaee11ec094e29c3fe80dd62a98
lmafia <lmafiamm@gmail.com>
2024-07-13 13:18:19 +0800

    intersection type

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,34 @@
-interface Person {
-    name: string;
-    age?: number; // 年龄是可选的
-    gender?: string; // 性别是可选的
+type Employee = {
+    name: string,
+    startDate: Date
+}
+type Admin = {
+    name: string,
+    privileges: string[]
 }
 
-// 可选属性影响匿名类和构造
-let person1: Person = { name: 'Alice' };
-let person2: Person = { name: 'Bob', age: 30 };
-let person3: Person = { name: 'Charlie', gender: 'male' };
+/**
+ * 要求 ElevatedEmployee 的变量对象要既符合 Admin 也符合 Employee
+ * type 也可以用 interface 替换 
+ * 这里的 & 就像是定义一个  ElevatedEmployee 接口
+ * 继承了 Admin 和 Employee
+ */
+type ElevatedEmployee = Admin & Employee;
+
+/**
+ * 如果用 | 
+ * 要求 ElevatedEmployee 的变量对象要既符合 Admin 或者 Employee 或者都符合
+ */
+// type ElevatedEmployee = Admin | Employee;
+
+const e: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['1'],
+    startDate: new Date()
+}
+
+
+
+type Combinable = string | number;
+type Numberic = number | boolean;
+type Universal = Combinable | Numberic;
