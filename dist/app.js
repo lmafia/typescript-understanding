d0ce402abbcb00d42de83fabe7225977c3325840
lmafia <lmafiamm@gmail.com>
2024-07-22 07:37:24 +0800

    Index Properties

--- dist/app.js
+++ dist/app.js
@@ -1,60 +1,12 @@
 "use strict";
-function logLength(x) {
-    if (typeof x === 'string') {
-        console.log(x.length); // x 在这个分支被 TypeScript 编译器识别为 string 类型
-    }
-    else {
-        console.log(x.toFixed(2)); // x 在这个分支被 TypeScript 编译器识别为 number 类型
-    }
-}
-logLength("hello"); // 输出: 5
-logLength(3.14159); // 输出: 3.14
-class Car {
-    drive() {
-        console.log("Driving the car");
-    }
-}
-class Truck {
-    drive() {
-        console.log("Driving the truck");
-    }
-    loadCargo() {
-        console.log("Loading cargo");
-    }
-}
-function driveVehicle(vehicle) {
-    if (vehicle instanceof Car) {
-        vehicle.drive(); // TypeScript 确定 vehicle 是 Car 类型
-    }
-    else {
-        vehicle.drive(); // TypeScript 确定 vehicle 是 Truck 类型
-        vehicle.loadCargo(); // TypeScript 确定 vehicle 是 Truck 类型
-    }
-}
-driveVehicle(new Car()); // 输出: Driving the car
-driveVehicle(new Truck()); // 输出: Driving the truck \n Loading cargo
-// pet is Bird 和单纯的 boolean 类型不一样
-// ts 可通过它来推导类型
-function isBird(pet) {
-    return pet.fly !== undefined;
-}
-function move(pet) {
-    if (isBird(pet)) {
-        pet.fly(); // TypeScript 确定 pet 是 Bird 类型
-    }
-    else {
-        pet.swim(); // TypeScript 确定 pet 是 Fish 类型
-    }
-}
-function printPersonInfo(person) {
-    if ('age' in person) {
-        console.log(`${person.name} is ${person.age} years old.`);
-    }
-    else {
-        console.log(`${person.name}'s age is unknown.`);
-    }
-}
-let person1 = { name: 'Alice', age: 30 };
-let person2 = { name: 'Bob' };
-printPersonInfo(person1); // 输出: Alice is 30 years old.
-printPersonInfo(person2); // 输出: Bob's age is unknown.
+const errorContainer = {
+    email: 'Not a vail email!',
+    userName: 'Must start with a captial character!'
+    // etc
+};
+const obj = {
+    name: "Alice",
+    age: 30,
+    address: "",
+    city: "Wonderland" // 允许添加额外的属性
+};
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
-const paragraph = document.getElementById('message-output');
-
-// ! 作用: 告诉 ts 这不会为 null; 使用 <> 进行类型转换, 因为 React 的 JSX 里也有使用 <> 
-// 所以 ts 推荐使用另外一种语法: as
-// const userInputEelemnt = <HTMLInputElement>document.getElementById('user-input')!;
-const userInputEelement = document.getElementById('user-input') as HTMLInputElement;
-
-userInputEelement.value = 'Hi there!'
-
-// 手动进行判空
-const rawInputElement = document.getElementById('user-input');
-if (rawInputElement) {
-    (rawInputElement as HTMLInputElement).value = 'Hi';
+interface ErrorContainer {
+    [porp: string]: string;
 }
+const errorContainer: ErrorContainer = {
+    email: 'Not a vail email!',
+    userName: 'Must start with a captial character!'
+    // etc
+}
+
+interface StringIndexed {
+    [key: string]: string | number;
+
+    // 可以定义一些已知的属性
+    name: string;
+    age: number;
+}
+
+const obj: StringIndexed = {
+    name: "Alice",
+    age: 30,
+    address: "",
+    city: "Wonderland"  // 允许添加额外的属性
+};
