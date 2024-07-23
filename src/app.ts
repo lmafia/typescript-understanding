928fd7f4672d2d169479df8bd6cd035a40ee0262
lmafia <lmafiamm@gmail.com>
2024-07-24 01:04:16 +0800

    Generic Class

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,31 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({ name: 'Mark' }, 'name'));
+
+
+
+class DataStorage<T> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data]
+    }
+
+}
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+console.log(numberStorage);
+
+const stringStorage = new DataStorage<string>();
+stringStorage.addItem('A');
+console.log(stringStorage);
+
